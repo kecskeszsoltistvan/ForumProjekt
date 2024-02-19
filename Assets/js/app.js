02d66b7215661feba18685e543517da652686d8f
Csoki <csokaandras@turr.hu>
2024-02-19 08:44:36 +0100

    Regisztráció angulárral való megjelenítése

--- Assets/js/app.js
+++ Assets/js/app.js
@@ -1,7 +1,20 @@
-var app = angular.module("forumApp", [])
+var app = angular.module("forumApp", ["ngRoute"])
 
 app.run(function($rootScope){
     $rootScope.title = "Fórum"
 
 
+})
+
+app.config(function($routeProvider){
+    $routeProvider
+    .when("/login", {
+        templateUrl: "Views/login.html",
+    })
+    .when("/reg", {
+        templateUrl: "./Views/registration.html",
+    })
+    .otherwise(
+        {redirecTo: "/login"}
+    )
 })
