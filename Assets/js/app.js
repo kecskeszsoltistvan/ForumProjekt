b3c5f011f429c68ac89fd039f2f8af9ea9d4015c
auerzoltan <80397127+auerzoltan@users.noreply.github.com>
2024-03-19 17:39:02 +0100

    legutobbi frissitesek dinamikussá tétele (nem kész)

--- Assets/js/app.js
+++ Assets/js/app.js
@@ -22,9 +22,67 @@ app.config(function($routeProvider){
     })
     .when("/legutobbi", {
         templateUrl: "Views/legutobbi.html",
+        controller: "../"
     })
     
     .otherwise(
         {redirecTo: "/login"}
     )
 })
+
+
+/*
+var listElement = document.getElementById('myList');
+let teszt = getChildren(document.getElementById('legutobbi'));
+
+if (window.getComputedStyle(listElement).display === 'none') {
+  listElement.style.display = 'none';
+} else {
+  listElement.style.display = 'block';
+}
+
+for (let index = 0; index < teszt.length; index++) {
+    if (index % 2 == 1) {
+        teszt[index].setAttribute("onClick", `test(${index + 1})`)
+    }
+}
+
+function getChildren(n, skipMe){
+    var r = [];
+    for ( ; n; n = n.nextSibling ) 
+       if ( n.nodeType == 1 && n != skipMe)
+          r.push( n );        
+    return r;
+};
+
+function getSiblings(n) {
+    return getChildren(n.parentNode.firstChild, n);
+    
+}
+
+function test(x){
+  let s = teszt[x-1];
+  let y = s.getElementsByTagName('img');
+  for (let index = 0; index < teszt.length; index++) {
+    if (index % 2 == 0) {
+      teszt[index].style.display = 'none';
+    }
+    else {
+      teszt[index].getElementsByTagName('img')[0].className = 'dropdown';
+    }
+  }
+
+
+  if (teszt[x].style.display === 'block' && y[0].className == 'dropdown flipped'){
+    teszt[x].style.display = 'none';
+    y[0].className = 'dropdown';
+  } else {
+    teszt[x].style.display = 'block';
+    y[0].className = 'dropdown flipped';
+  }
+}
+*/
+
+
+
+
