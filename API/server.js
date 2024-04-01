cfe474da29e78d5fdf358b308a42008a03730615
Bandi <csokaandras@turr.hu>
2024-04-01 18:08:38 +0200

    Post, patch category

--- API/server.js
+++ API/server.js
@@ -1,10 +1,10 @@
-require("dotenv").config()
-var mysql = require('mysql')
-const express = require('express')
-const cors = require('cors')
-const app = express()
-const port = process.env.PORT
-app.use(cors())
+require("dotenv").config();
+var mysql = require('mysql');
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = process.env.PORT;
+app.use(cors());
 
 var pool  = mysql.createPool({
     connectionLimit : 10,
@@ -16,11 +16,11 @@ var pool  = mysql.createPool({
 
 // MIDDLEWARES
 app.use(express.urlencoded({extended: true}))
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors());
 
 app.get('/', function (req, res) {
-  res.send('Backend APi from Kisbarát to Neked')
+  res.send('Backend APi from Kisbarát to Neked');
 })
 
 // --------------------
@@ -47,42 +47,42 @@ app.get("/users/:pk", cors(), (req, res)=>{
 */
 // POST new user
 app.post("/users", cors(), (req, res)=>{
-    let data  = req.body
+    let data  = req.body;
     pool.query(`INSERT INTO users (id, name, email, password) VALUES(NULL, "${data.name}", "${data.email}", "${data.password}")`, (error, results)=>{
         if (error) throw res.status(500).send(error);
-        res.status(200).send(results)
+        res.status(200).send(results);
     });
 
 })
 
 // login
 app.get("/login/:email", cors(), (req, res)=>{
-    let data  = req.params.email
+    let data  = req.params.email;
     pool.query(`SELECT * FROM users WHERE email = "${data}"`, (error, results)=>{
         if (error) throw res.status(500).send(error);
-        res.status(200).send(results)
+        res.status(200).send(results);
     });
 
 })
 
 // PATCH one user by PK
 app.patch("/users/:pk", cors(), (req, res)=>{
-    let pk = req.params.pk
-    let data  = req.body
+    let pk = req.params.pk;
+    let data  = req.body;
     pool.query(`UPDATE users SET name="${data.name}", email="${data.email}", password="${data.password}" WHERE ID=?`, pk, (error, results)=>{
-        if (error) throw res.status(500).send(error)
-        res.status(200).send(results)
+        if (error) throw res.status(500).send(error);
+        res.status(200).send(results);
     });
 
 })
 
 // DELETE one user by PK
 app.delete("/users/:pk", cors(), (req, res)=>{
-    let pk = req.params.pk
+    let pk = req.params.pk;
     pool.query(`DELETE FROM users WHERE ID=?`, pk, (error, results)=>{
         if (error) throw res.send(error);
         
-        res.send(results)
+        res.send(results);
     });
 })
 
@@ -91,7 +91,7 @@ app.get("/:table", cors(), (req, res)=>{
     let table = req.params.table;
     pool.query(`SELECT * FROM ${table}`, (error, results)=>{
         if (error) throw res.send(error);
-        res.send(results)
+        res.send(results);
     });
 })
 
@@ -104,7 +104,7 @@ app.get("/:table/:field/:op/:value", cors(), (req, res)=>{
 
     op = getOp(op);
     if(op == "like"){
-        value = `%${value}$`
+        value = `%${value}$`;
     }
 
     pool.query(`SELECT * FROM ${table} WHERE ${field} ${op} '${value}' `, (error, results)=>{
@@ -124,11 +124,11 @@ app.delete("/:table/:id", cors(), (req, res)=>{
     })
 })
 
-//GEt posts from category
+//GET posts from category
 app.get("/posts/category/:id", cors(), (req, res)=>{
     let id = req.params.id;
 
-    pool.query(`SELECT posts.ID, posts.user_id, posts.category_id, posts.title, posts.text, posts.created_at FROM posts JOIN categorys ON posts.category_id = categorys.ID WHERE posts.category_id = ${id}`, (error, results)=>{
+    pool.query(`SELECT posts.ID, posts.user_id, posts.category_id, posts.title, posts.text, posts.created_at FROM posts JOIN categories ON posts.category_id = categories.ID WHERE posts.category_id = ${id}`, (error, results)=>{
         if (error) throw res.send(error);
         res.send(results);
     })
@@ -148,17 +148,51 @@ app.post("/posts/category/:id", cors(), (req, res)=>{
 
 // PATCH post by PK
 app.patch("/posts/:id", cors(), (req, res)=>{
-    let id = req.params.id
-    let data  = req.body
+    let id = req.params.id;
+    let data  = req.body;
     
     pool.query(`UPDATE posts SET user_id=${data.user_id}, category_id=${data.category_id}, title="${data.title}", text="${data.text}", created_at="${data.created_at}" WHERE ID=${id}`, (error, results)=>{
-        if (error) throw res.status(500).send(error)
-        res.status(200).send(results)
+        if (error) throw res.status(500).send(error);
+        res.status(200).send(results);
     });
 
 })
 // Itt velem szemben ez a nő úgy néz ki mint az Irén
 // kicsit kajak:DD
+
+// POST new category
+app.post("/categories", cors(), (req, res)=>{
+    let data = req.body;
+
+    pool.query(`INSERT INTO categories (ID, title, body, created_at) VALUES(NULL, "${data.title}", "${data.body}", "${data.created_at}")`, (error, results)=>{
+        if (error) throw res.status(500).send(error);
+        res.status(200).send(results);
+    })
+})
+
+// PATCH category by id
+app.patch("/categories/:id", cors(), (req, res)=>{
+    let id = req.params.id;
+    let data = req.body;
+
+    pool.query(`UPDATE categories SET title="${data.title}", body="${data.body}", created_at="${data.created_at}" WHERE ID=${id}`, (error, results)=>{
+        if (error) throw res.status(500).send(error);
+        res.status(200).send(results);
+    })
+})
+
+/*
+// DELETE category and posts by categry id
+app.delete("/category/posts/:id", cors(), (req, res)=>{
+    let id = req.params.id;
+
+    pool.query(`DELETE FROM posts WHERE category_id=${id} DELETE FROM categories WHERE ID=${id}`, (error, results)=>{
+        if (error) throw res.status(500).send(error);
+        res.status(200).send(results);
+    })
+})
+*/
+
 function getOp(op){
     switch(op){
       case 'eq': {op = '='; break}
@@ -173,5 +207,5 @@ function getOp(op){
   }
 
 app.listen(port, ()=>{
-    console.log(`A szerver hallgatózik a ${port} porton...`)
+    console.log(`Én csak itt hallgatózok a ${port} porton...`);
 })
